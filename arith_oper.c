968be117cb0a2c5ba0f4bffffa016eda9e6e6faf
janani <rahuljanani.pandi@votarytech.com>
2018-09-06 17:00:16 +0530

    adding arithematic addition

--- arith_oper.c
+++ arith_oper.c
@@ -5,10 +5,16 @@ int main()
 int a=10,b=20,c;
 c=add(a,b);
 printf("addition is %d\n",c);
+c=sub(a,b);
 return 0;
 }
 int add(int x,int y)
 {
 int z;
 return x+y;
+}
+int sub(int x,int y)
+{
+int z;
+return x-y;
 }
