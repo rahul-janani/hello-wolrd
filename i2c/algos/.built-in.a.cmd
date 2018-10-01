c35266c2a69351ae7d2e1da5ad8c83f3288435db
janani <rahuljanani.pandi@votarytech.com>
2018-10-01 17:35:25 +0530

    remove i2c

--- i2c/algos/.built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/i2c/algos/built-in.a := rm -f drivers/i2c/algos/built-in.a; /home/pandirah/target1/qemu/build/../toolchains/aarch64/bin/aarch64-linux-gnu-ar rcSTPD drivers/i2c/algos/built-in.a drivers/i2c/algos/i2c-algo-bit.o
